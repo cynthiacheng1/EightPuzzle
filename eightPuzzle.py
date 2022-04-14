703e204f3fa6bb85780b69b1a06f4d4b43799264
Cynthia Cheng <cynthia.cheng@Cynthias-MacBookPro.local>
2022-04-14 16:36:18 -0400

    update

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -24,7 +24,7 @@ def findCoordinates(num,board):
             if (num == board[i][j]):
                 return i,j
 
-print(findCoordinates(1,initial))
+# print(findCoordinates(1,initial))
 
 def calculateManhattan(initial, goal):
     total = 0
@@ -33,6 +33,41 @@ def calculateManhattan(initial, goal):
         goal_x, goal_y = findCoordinates(i,goal)
         total += abs(initial_x-goal_x) + abs(goal_y-initial_y)
     return total
-print(calculateManhattan(initial, goal))
+# print(calculateManhattan(initial, goal))
+
+
+clockwiseOrder = [[0,0],[0,1],[0,2],[1,2],[2,2],[2,1],[2,0],[1,0]]
+
+def findNext(num,board):
+    num_x, num_y = findCoordinates(num,board)
+    if (num_x == 1 and num_y == 0):
+        return board[0][0]
+    else:
+        for j in range(len(clockwiseOrder)):
+            if (clockwiseOrder[j][0] == num_x and clockwiseOrder[j][1] == num_y):
+                coords = clockwiseOrder[j+1]
+                xcord = coords[0]
+                ycord = coords[1]
+                return board[xcord][ycord]
+# print(findNext(1,initial))
+
+
+def clockWise(initial, goal):
+    total = 0
+    for c in range(len(clockwiseOrder)):
+        num = initial[clockwiseOrder[c][0]][clockwiseOrder[c][1]]
+        if (num != 0):
+            nextIntialNum = findNext(num, initial)
+            nextGoalNum = findNext(num, goal)
+            if (nextIntialNum != nextGoalNum):
+                total += 2
+    if (initial[1][1] != goal[1][1]):
+        total += 1
+    return total
+        
+print(clockWise(initial, goal))
+
+def h2(intial,goal):
+    return 3*clockWise(initial,goal) + calculateManhattan(intial,goal)
 
 
