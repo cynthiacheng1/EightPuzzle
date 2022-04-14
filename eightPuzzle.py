820e785d163204600cd453e2f936e6b17194267f
matt-swartz <mattcswartz@gmail.com>
2022-04-14 17:36:41 -0400

    matt

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -4,6 +4,7 @@ with open("Input1.txt") as file:
 
 initial = []
 goal = []
+clockwiseOrder = [[0,0],[0,1],[0,2],[1,2],[2,2],[2,1],[2,0],[1,0]]
 
 for i in range(len(lines)):
     if (i < 3):
@@ -15,62 +16,137 @@ for i in range(len(lines)):
         #     goal.append(int(x))
         goal.append([int(x) for x in lines[i].split()])
 
-print(initial)
-print(goal)
-
-def findCoordinates(num,board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if (num == board[i][j]):
-                return i,j
-
-# print(findCoordinates(1,initial))
-
-def calculateManhattan(initial, goal):
-    total = 0
-    for i in range(1,9):
-        initial_x, initial_y = findCoordinates(i,initial)
-        goal_x, goal_y = findCoordinates(i,goal)
-        total += abs(initial_x-goal_x) + abs(goal_y-initial_y)
-    return total
-# print(calculateManhattan(initial, goal))
-
-
-clockwiseOrder = [[0,0],[0,1],[0,2],[1,2],[2,2],[2,1],[2,0],[1,0]]
-
-def findNext(num,board):
-    num_x, num_y = findCoordinates(num,board)
-    if (num_x == 1 and num_y == 0):
-        return board[0][0]
-    else:
-        for j in range(len(clockwiseOrder)):
-            if (clockwiseOrder[j][0] == num_x and clockwiseOrder[j][1] == num_y):
-                coords = clockwiseOrder[j+1]
-                xcord = coords[0]
-                ycord = coords[1]
-                return board[xcord][ycord]
-# print(findNext(1,initial))
-
-
-def clockWise(initial, goal):
-    total = 0
-    for c in range(len(clockwiseOrder)):
-        num = initial[clockwiseOrder[c][0]][clockwiseOrder[c][1]]
-        if (num != 0):
-            nextIntialNum = findNext(num, initial)
-            nextGoalNum = findNext(num, goal)
-            if (nextIntialNum != nextGoalNum):
-                total += 2
-    if (initial[1][1] != goal[1][1]):
-        total += 1
-    return total
+class Node:
+    def __init__(self, board, level, fval):
+        self.board = board
+        self.level = level
+        self.fval = fval
         
-print(clockWise(initial, goal))
+    def generate_child(self):
+        x, y = self.find(self.board, 0)
+        val_list = [[x, y - 1],[x, y + 1], [x - 1, y], [x + 1, y]]
+        children = []
+        for i in val_list:
+            child = self.shuffle(self.board, x, y, i[0], i[1])
+            if child is not None:
+                child_node = Node(child, self.level + 1, 0)
+                children.append(child_node)
+        return children
+    
+    def shuffle(self, puz, x1, y1, x2, y2):
+        if x2 >= 0 and x2 < len(self.board) and y2 >= 0 and y2 < len(self.board):
+            temp_puz = []
+            temp_puz = self.copy(puz)
+            temp = temp_puz[x2][y2]
+            temp_puz[x2][y2] = temp_puz[x1][y1]
+            temp_puz[x1][y1] = temp
+            return temp_puz
+        else:
+            return None
+            
 
-def h1(initial,goal):
-    return calculateManhattan(intial,goal)
+    def copy(self, root):
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp    
+            
+    def find(self, puz, x):
+        for i in range(0,len(self.board)):
+            for j in range(0,len(self.board)):
+                if puz[i][j] == x:
+                    return i,j
 
-def h2(intial,goal):
-    return 3*clockWise(initial,goal) + calculateManhattan(intial,goal)
 
+class Puzzle:
+    def __init__(self):
+        self.open = []
+        self.closed = []
+
+    def findCoordinates(self, num, board):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if (num == board[i][j]):
+                    return i,j
+
+    def calculateManhattan(self, initial, goal):
+        total = 0
+        for i in range(1,9):
+            initial_x, initial_y = findCoordinates(i,initial)
+            goal_x, goal_y = findCoordinates(i,goal)
+            total += abs(initial_x-goal_x) + abs(goal_y-initial_y)
+        return total
+
+    def findNext(self, num, board):
+        num_x, num_y = findCoordinates(num,board)
+        if (num_x == 1 and num_y == 0):
+            return board[0][0]
+        else:
+            for j in range(len(clockwiseOrder)):
+                if (clockwiseOrder[j][0] == num_x and clockwiseOrder[j][1] == num_y):
+                    coords = clockwiseOrder[j+1]
+                    xcord = coords[0]
+                    ycord = coords[1]
+                    return board[xcord][ycord]
+
+    def clockWise(self, initial, goal):
+        total = 0
+        for c in range(len(clockwiseOrder)):
+            num = initial[clockwiseOrder[c][0]][clockwiseOrder[c][1]]
+            if (num != 0):
+                nextIntialNum = findNext(num, initial)
+                nextGoalNum = findNext(num, goal)
+                if (nextIntialNum != nextGoalNum):
+                    total += 2
+        if (initial[1][1] != goal[1][1]):
+            total += 1
+        return total
+
+    def h1(self, initial, goal):
+        return calculateManhattan(intial,goal)
+
+    def h2(self, intial, goal):
+        return 3*clockWise(initial,goal) + calculateManhattan(intial,goal)
+
+    def f(self,start,goal):
+        return h1(start, goal) + start.level        
+
+    def process(self):
+        start = initial
+        self.goal = goal
+        
+        start = Node(start,0,0)
+        start.fval = self.f(start,goal)
+        
+        self.open.append(start)
+        print("\n\n")
+        while True:
+            cur = self.open[0]
+            print("")
+            print("  | ")
+            print("  | ")
+            print(" \\\'/ \n")
+            for i in cur.board:
+                for j in i:
+                    print(j,end=" ")
+                print("")
+            """ If the difference between current and goal node is 0 we have reached the goal node"""
+            if(self.h(cur.board,goal) == 0):
+                break
+            for i in cur.generate_child():
+                i.fval = self.f(i,goal)
+                self.open.append(i)
+            self.closed.append(cur)
+            del self.open[0]
+
+            """ sort the opne list based on f value """
+            self.open.sort(key = lambda x:x.fval,reverse=False)
+
+
+puz = Puzzle()
+puz.process()
+        
 
